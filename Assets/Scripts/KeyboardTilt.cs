268222c2305b11a61f32751d3feae53c8c415897
Seaney Shell <afc76927@opayq.com>
2019-04-03 14:42:43 -0700

    :wq

---
+++ Assets/Scripts/KeyboardTilt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTilt : MonoBehaviour
+{
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(-5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 5f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -5f);
+        }
+    }
+
+}
