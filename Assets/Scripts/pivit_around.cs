dd2d4beb6bd057d435dfcbbf916cfe86582279b5
Seaney Shell <afc76927@opayq.com>
2019-04-04 22:39:50 -0700

    added documentation to the pivit_around script used for insuing pivit happens around the user rather then a point, as well as updaing a test scene named testing_mats

--- Assets/Scripts/pivit_around.cs
+++ Assets/Scripts/pivit_around.cs
@@ -2,29 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * To use this your map must be in the structure of with all ->'s meaning "parent of"
+ * GameObject pivit -> GameObject map -> all objects to be rotated by user input.   
+ * 
+ * Note : this can cuase some issues on highly vertical maps and when the ball gets a large distance away from the map, 
+ *        so Stoping exacution quickly after a level fails or ball leaves the bounds may be a good idea
+ * 
+ * 
+ */
+
 public class pivit_around : MonoBehaviour
 {
-    public Transform target;
-    public Transform pivit; 
-    public Transform map;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
+    public Transform target; // set this to the player's transform
+    public Transform pivit;  // set this to the top level pivit objects transform
+    public Transform map; // set this to the map objects transform 
+    public bool active;
+
+
+
+    // this insures the world will pivit around the player
     private void LateUpdate()
     {
         pivit.position = new Vector3(target.position.x, 0, target.position.z);
-        map.position = new Vector3(-target.position.x, 0,-target.position.z);
-        //target.position = Vector3.zero;
-
+        map.position = new Vector3(-target.position.x, 0, -target.position.z);
     }
+
 }
