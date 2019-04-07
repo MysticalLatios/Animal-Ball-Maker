88ccbfd7b7fa4f2c2b8de691904cebab9449ea92
Seaney Shell <afc76927@opayq.com>
2019-04-07 06:37:45 -0700

    added a script to spawn point that creates a player prefab when the scene is loaded for #8, this cuases odd interactions with the current camra controller this may need to be worked on as a team

---
+++ Assets/Scripts/Start_prefab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_prefab : MonoBehaviour
+{
+
+    public GameObject PlayePrefab;
+    GameObject PlayerInstance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInstance = Instantiate(PlayePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        //Camera.main.transform.position = new Vector3(-3, 8, 0);
+        Camera.main.GetComponent<CamraController>().player = PlayerInstance;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
